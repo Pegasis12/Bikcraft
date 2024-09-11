1fb4271b93ca9f2a55fb2ff7c6840744bc18e659
Pablo Lucas Santana Queiroz <pablo.queiroz@HINOVA.net>
2024-09-11 15:59:49 -0300

    galeria

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ativar o after do a ao ser clicado
+//menu header para ativar o after do a ao ser clicado
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -12,3 +12,56 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//ativar itens do orçamento
+// location acompanhado com .serach pega os parametros que estão no link da url, a classe URLSearchParams pega esses parametros sem precisa manipular a String, mas a classe precisa acompanhar o new antes e dentro do parametro a string no caso o location.search
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  // elemento pega o id qu o parametro esta atribuido
+  const elemento = document.getElementById(parametro);
+  // if verifica se o parametro existe, se existir mais de um ou algo rolar ele não da erro e retorna null
+  if (elemento) {
+    // elemento.cheked faz o html checar a propiedade
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function EventosPergunta(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(EventosPergunta);
+
+//galeria de imagens biciletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(imagem) {
+  imagem.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
